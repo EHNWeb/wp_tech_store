f6231efc240c9d24b94d909fd69e16ab91227c5c
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-09-16 14:31:25 +0200

    Add category

---
+++ wp-content/themes/monpremiertheme/category.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+<div class="row justify-content-center">
+    <div class="col-md-10 shadow p-4 my-5">
+        <!-- Début de la Boucle. -->
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                <!-- Affiche le Titre en tant que lien vers le Permalien de l'Article. -->
+                <h2 class="titre-page my-5"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+                <!-- Affiche le corps (Content) de l'Article dans un bloc div. -->
+                <div class="entry">
+                    <?php the_content(); ?>
+                </div>
+
+                <!-- Fin de La Boucle (mais notez le "else:" - voir la suite). -->
+            <?php endwhile;
+        else : ?>
+
+            <!-- Le premier "if" testait l'existence d'Articles à afficher. Cette -->
+            <!-- partie "else" indique que faire si ce n'est pas le cas. -->
+            <p>Désolé, ce contenu n'existe pas.</p>
+
+            <!-- Fin REELLE de La Boucle. -->
+        <?php endif; ?>
+    </div>
+</div>
+<?php get_footer(); ?>
